46afc6dca902ad8135975e6fd6f0a9950acac279
Andrew Wong <ndrwchkwng@yahoo.com>
2020-03-04 12:42:13 -0500

    first commit

---
+++ Crawler.sql
@@ -0,0 +1,38 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema Crawler
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `Crawler` ;
+
+-- -----------------------------------------------------
+-- Schema Crawler
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `Crawler` DEFAULT CHARACTER SET utf8 ;
+USE `Crawler` ;
+
+-- -----------------------------------------------------
+-- Table `Record`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Record` ;
+
+CREATE TABLE IF NOT EXISTS `Record` (
+  `idRecord` INT NOT NULL AUTO_INCREMENT,
+  `URL` BLOB NOT NULL,
+  PRIMARY KEY (`idRecord`))
+ENGINE = InnoDB;
+
+SET SQL_MODE = '';
+DROP USER IF EXISTS user@localhost;
+SET SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+CREATE USER 'user'@'localhost' IDENTIFIED BY 'admin';
+
+GRANT ALL ON * TO 'user'@'localhost';
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
