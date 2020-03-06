db7a0bcbd4d5d88ae862194d9080f1157ad9aab6
Andrew Wong <ndrwchkwng@yahoo.com>
2020-03-06 10:49:02 -0500

    can find URLs on 1 page

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -19,7 +22,7 @@ public class Main {
 	}
 
 	private static void processPage(String URL) throws SQLException, IOException {
-		System.out.println(URL);
+		
 		//checks if URL is already in db
 		String sql = "select * from Record where URL = '"+URL+"'";
 		ResultSet rs = db.runSql(sql);
@@ -31,6 +34,24 @@ public class Main {
 			PreparedStatement stmt = db.conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 			stmt.setString(1, URL);
 			stmt.execute();
+			
+			System.out.println(URL);
+			
+			//Print to txt file cochrane_reviews.txt
+//			try {
+//				FileWriter fw = new FileWriter("cochrane_reviews.txt");
+//				PrintWriter pw = new PrintWriter(fw);
+//			
+//					for (String string : URL) {
+//						pw.println(string);
+//					}
+//			pw.close();
+//			}
+//			catch (IOException e) {
+//				e.printStackTrace();
+//			}
+		
+			
 			
 			//use Jsoup
 			Document doc = Jsoup.connect("https://www.cochranelibrary.com/cdsr/reviews/topics").get();
